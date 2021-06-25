5deec8a045decc3b8caf04f07e63958552ae7ed5
francisscah <85348624+francisscah@users.noreply.github.com>
2021-06-25 10:22:20 +0300

    Add files via upload

---
+++ karitho.cpp
@@ -0,0 +1,13 @@
+//program for area of a circle
+#include <iostream>
+using namespace std;
+int main()
+{
+	int radius,area;
+	float pi=3.142;
+	cout<<"enter radius";
+	cin>>radius;
+	area=pi*radius*radius;
+	cout<<"area="<<area;
+	return 0;
+}
